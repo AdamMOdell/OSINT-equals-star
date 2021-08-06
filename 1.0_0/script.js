de9d17f9229ec9d1d7f1c62837d5e92822ae6ef3
Adam Odell <AdamMOdell@users.noreply.github.com>
2021-08-06 12:53:21 -0700

    added abuseipdb, talos, xforce, ipinfo, greynoise

--- 1.0_0/script.js
+++ 1.0_0/script.js
@@ -7,7 +7,12 @@ var menuId = chrome.contextMenus.create({
 
 function main(info, tab) {
   chrome.windows.create({
-      url: `https://www.virustotal.com/gui/search/${info.selectionText.trim()}`,
+      url: [`https://www.virustotal.com/gui/search/${info.selectionText.trim()}`,
+			`https://www.abuseipdb.com/check/${info.selectionText.trim()}`,
+			`https://talosintelligence.com/reputation_center/lookup?search=${info.selectionText.trim()}`,
+			`https://exchange.xforce.ibmcloud.com/search/${info.selectionText.trim()}`,
+			`https://ipinfo.io/${info.selectionText.trim()}`,
+			`https://www.greynoise.io/viz/riot/${info.selectionText.trim()}`],
       incognito: false,
     })
 }
