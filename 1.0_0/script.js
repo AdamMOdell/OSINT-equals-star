0179c06314ac130b5cc5c900e8a37d1d41f5b0e8
Shadyk88 <88535940+Shadyk88@users.noreply.github.com>
2021-08-07 01:40:19 -0700

    Update script.js
    Added hash search w regex

--- 1.0_0/script.js
+++ 1.0_0/script.js
@@ -9,6 +9,21 @@ function main(info, tab) {
 	var IOC = info.selectionText;
 	IOC = IOC.replace(/[\"\']/g, '');
 	IOC = IOC.trim();
+	
+	var ishash = !IOC.search(/\b[A-Fa-f0-9]{32,64}\b/); // run hash search with regex
+	
+	if (ishash)
+	
+chrome.windows.create({
+      url: [`https://www.virustotal.com/gui/search/${info.selectionText.trim()}`,
+			`https://talosintelligence.com/reputation_center/lookup?search=${info.selectionText.trim()}`,
+			`https://exchange.xforce.ibmcloud.com/search/${info.selectionText.trim()}`,
+			`https://www.hybrid-analysis.com/search?query=${info.selectionText.trim()}`],
+      incognito: false,
+    })
+	
+	else
+	
   chrome.windows.create({
       url: [`https://www.virustotal.com/gui/search/${IOC}`,
 			`https://www.abuseipdb.com/check/${IOC}`,
