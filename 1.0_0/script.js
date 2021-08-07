d3e5f9ba5ad579e7de582f739cc46ca98823aaea
Adam Odell <AdamMOdell@users.noreply.github.com>
2021-08-07 01:25:14 -0700

    filters out whitespace and quotes

--- 1.0_0/script.js
+++ 1.0_0/script.js
@@ -6,13 +6,16 @@ var menuId = chrome.contextMenus.create({
 })
 
 function main(info, tab) {
+	var IOC = info.selectionText;
+	IOC = IOC.replace(/[\"\']/g, '');
+	IOC = IOC.trim();
   chrome.windows.create({
-      url: [`https://www.virustotal.com/gui/search/${info.selectionText.trim()}`,
-			`https://www.abuseipdb.com/check/${info.selectionText.trim()}`,
-			`https://talosintelligence.com/reputation_center/lookup?search=${info.selectionText.trim()}`,
-			`https://exchange.xforce.ibmcloud.com/search/${info.selectionText.trim()}`,
-			`https://ipinfo.io/${info.selectionText.trim()}`,
-			`https://www.greynoise.io/viz/riot/${info.selectionText.trim()}`],
+      url: [`https://www.virustotal.com/gui/search/${IOC}`,
+			`https://www.abuseipdb.com/check/${IOC}`,
+			`https://talosintelligence.com/reputation_center/lookup?search=${IOC}`,
+			`https://exchange.xforce.ibmcloud.com/search/${IOC}`,
+			`https://ipinfo.io/${IOC}`,
+			`https://www.greynoise.io/viz/riot/${IOC}`],
       incognito: false,
-    })
+    });
 }
