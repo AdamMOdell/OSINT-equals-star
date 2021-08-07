9c9cff3c01bda96bf21661d6000125d8bae10adc
Adam Odell <AdamMOdell@users.noreply.github.com>
2021-08-07 01:59:01 -0700

    added domains

--- 1.0_0/script.js
+++ 1.0_0/script.js
@@ -7,30 +7,38 @@ var menuId = chrome.contextMenus.create({
 
 function main(info, tab) {
 	var IOC = info.selectionText;
-	IOC = IOC.replace(/[\"\']/g, '');
-	IOC = IOC.trim();
+	IOC = IOC.replace(/[\"\'\[\] ]/g, '');
 	
 	var ishash = !IOC.search(/\b[A-Fa-f0-9]{32,64}\b/); // run hash search with regex
-	
-	if (ishash)
-	
-chrome.windows.create({
-      url: [`https://www.virustotal.com/gui/search/${info.selectionText.trim()}`,
-			`https://talosintelligence.com/reputation_center/lookup?search=${info.selectionText.trim()}`,
-			`https://exchange.xforce.ibmcloud.com/search/${info.selectionText.trim()}`,
-			`https://www.hybrid-analysis.com/search?query=${info.selectionText.trim()}`],
-      incognito: false,
-    })
-	
-	else
-	
-  chrome.windows.create({
-      url: [`https://www.virustotal.com/gui/search/${IOC}`,
-			`https://www.abuseipdb.com/check/${IOC}`,
-			`https://talosintelligence.com/reputation_center/lookup?search=${IOC}`,
-			`https://exchange.xforce.ibmcloud.com/search/${IOC}`,
-			`https://ipinfo.io/${IOC}`,
-			`https://www.greynoise.io/viz/riot/${IOC}`],
-      incognito: false,
-    });
+	var isIP = !IOC.search(/^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/);
+	if (ishash){ // hash
+		chrome.windows.create({
+		  url: [`https://www.virustotal.com/gui/search/${IOC}`,
+				`https://talosintelligence.com/reputation_center/lookup?search=${IOC}`,
+				`https://exchange.xforce.ibmcloud.com/search/${IOC}`,
+				`https://www.hybrid-analysis.com/search?query=${IOC}`],
+		  incognito: false,
+		})
+	}
+	else if (isIP){
+		chrome.windows.create({
+		  url: [`https://www.virustotal.com/gui/search/${IOC}`,
+				`https://www.abuseipdb.com/check/${IOC}`,
+				`https://talosintelligence.com/reputation_center/lookup?search=${IOC}`,
+				`https://exchange.xforce.ibmcloud.com/search/${IOC}`,
+				`https://ipinfo.io/${IOC}`,
+				`https://www.greynoise.io/viz/riot/${IOC}`,
+				`https://www.shodan.io/search?query=${IOC}`],
+		  incognito: false,
+		});
+	}
+	else{
+		chrome.windows.create({
+		  url: [`https://www.virustotal.com/gui/search/${IOC}`,
+				`https://talosintelligence.com/reputation_center/lookup?search=${IOC}`,
+				`https://exchange.xforce.ibmcloud.com/search/${IOC}`,
+				`https://www.shodan.io/search?query=${IOC}`],
+		  incognito: false,
+		});
+	}
 }
